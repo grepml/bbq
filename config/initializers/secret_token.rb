d1818269749f837791f5b4d0682bb322a5c4f3df
GrepML <grep.ml@gmail.com>
2013-07-14 21:40:29 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbq::Application.config.secret_token = '9afbf50de8fff6295b9371f1f2d2291bec94c5acad23966758e318b87111ae39a99805604d8fd8762361eecad76befb8b5f81d8ff27d3ce0ac9dc655cd4ba798'
